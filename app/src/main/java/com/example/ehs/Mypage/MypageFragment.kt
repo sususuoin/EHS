0ae810040dee3698f1a5351ae0a6bbd938dec2bd
sususuoin <tndlstksxk@naver.com>
2021-06-19 21:56:07 +0900

    마이페이지에 원형그래프 추가

--- app/src/main/java/com/example/ehs/Mypage/MypageFragment.kt
+++ app/src/main/java/com/example/ehs/Mypage/MypageFragment.kt
@@ -8,21 +8,35 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.icu.lang.UCharacter.IndicPositionalCategory.TOP
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.util.Size
+import android.view.Gravity.TOP
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams.TOP
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.example.ehs.Feed.Community
 import com.example.ehs.Login.AutoLogin
 import com.example.ehs.Login.LoginActivity
 import com.example.ehs.R
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.fragment_mypage.*
 import kotlinx.android.synthetic.main.fragment_mypage.view.*
 
@@ -31,20 +45,21 @@ class MypageFragment : Fragment() {
     private var a: Context? = null
 
 
-    lateinit var tv_id : TextView
-    lateinit var tv_pw : TextView
-    lateinit var tv_name : TextView
-    lateinit var tv_email : TextView
-    lateinit var tv_birth : TextView
-    lateinit var tv_gender : TextView
-    lateinit var tv_level : TextView
-    lateinit var iv_profileimg : ImageView
+    lateinit var tv_id: TextView
+    lateinit var tv_pw: TextView
+    lateinit var tv_name: TextView
+    lateinit var tv_email: TextView
+    lateinit var tv_birth: TextView
+    lateinit var tv_gender: TextView
+    lateinit var tv_level: TextView
+    lateinit var iv_profileimg: ImageView
 
     lateinit var modifybtn: ImageButton
+    lateinit var pieChart: PieChart
 
     companion object {
-        const val TAG : String = "마이페이지 프레그먼트"
-        fun newInstance() : MypageFragment { // newInstance()라는 함수를 호출하면 HomeFragment를 반환함
+        const val TAG: String = "마이페이지 프레그먼트"
+        fun newInstance(): MypageFragment { // newInstance()라는 함수를 호출하면 HomeFragment를 반환함
             return MypageFragment()
         }
     }
@@ -54,6 +69,8 @@ class MypageFragment : Fragment() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "MypageFragment - onCreate() called")
     }
+
+
     // 프레그먼트를 안고 있는 액티비티에 붙었을 때
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -74,12 +91,16 @@ class MypageFragment : Fragment() {
     ): View? {
         Log.d(TAG, "MypageFragment - onCreateView() called")
         val view: View = inflater!!.inflate(R.layout.fragment_mypage, container, false)
+        pieChart = view.findViewById(R.id.piechart_mypage)
+
 
         modifybtn = view.findViewById(R.id.btn_modify)
-        modifybtn.setOnClickListener{
-            activity?.let{
+        modifybtn.setOnClickListener {
+            activity?.let {
                 val intent = Intent(context, UserModifyActivity::class.java)
-                startActivity(intent) }
+                startActivity(intent)
+            }
+
         }
 
         view.btn_logout.setOnClickListener { view ->
@@ -130,7 +151,6 @@ class MypageFragment : Fragment() {
 //        userProfile = resize(userProfile!!)
 
 
-
         //fragment1의 TextView에 전달 받은 text 띄우기
         tv_id.text = userId
         tv_pw.text = userPw
@@ -142,6 +162,10 @@ class MypageFragment : Fragment() {
 
 //        iv_profileimg.setImageResource(R.drawable.exfirst)
         iv_profileimg.setImageBitmap(userProfile)
+
+        setupPieChart()
+        loadPieChartData()
+
 
 
 
@@ -183,5 +207,126 @@ class MypageFragment : Fragment() {
     }
 
 
+    private fun setupPieChart() {
+        with(pieChart) {
+            isDrawHoleEnabled = true
+            setUsePercentValues(true)
+            setEntryLabelTextSize(10f)
+            setEntryLabelColor(Color.BLACK)
+//            centerText = "방가방가햄토리즈"
+            setCenterTextSize(15f)
+            description.isEnabled = false
+            setExtraOffsets(5f, 10f, 5f, 5f)
+//            setHoleColor(Color.WHITE)
+            transparentCircleRadius = 61f
+//            rawRotationAngle = 50f
 
+
+//범례(오른쪽 상단)
+
+            val legend: Legend = pieChart.getLegend()
+//            legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP // 범례
+//            legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT // 화면과 라벨 방향
+//            legend.orientation = Legend.LegendOrientation.VERTICAL
+//            legend.setDrawInside(false)
+            legend.isEnabled = false
+
+
+        }
+    }
+
+
+    fun loadPieChartData() {
+
+        val entries: ArrayList<PieEntry> = ArrayList()
+        with(entries) {
+
+            add(PieEntry(1f, "흰색"))
+            add(PieEntry(2f, "크림색"))
+            add(PieEntry(4f, "연회색"))
+            add(PieEntry(4f, "진회색"))
+            add(PieEntry(4f, "검정"))
+
+            add(PieEntry(4f, "주황"))
+            add(PieEntry(4f, "베이지"))
+            add(PieEntry(4f, "노랑"))
+            add(PieEntry(4f, "연두"))
+            add(PieEntry(4f, "하늘"))
+
+            add(PieEntry(4f, "분홍"))
+            add(PieEntry(4f, "연분홍"))
+            add(PieEntry(4f, "초록"))
+            add(PieEntry(4f, "카키"))
+            add(PieEntry(4f, "파랑"))
+
+            add(PieEntry(4f, "빨강"))
+            add(PieEntry(4f, "와인"))
+            add(PieEntry(4f, "갈색"))
+            add(PieEntry(4f, "보라"))
+            add(PieEntry(4f, "네이비"))
+
+
+
+        }
+        val colors: ArrayList<Int> = ArrayList()
+        val colorstwo: ArrayList<Int> = ArrayList()
+        with(colorstwo) {
+
+            add(Color.parseColor("#FFFFFFFF")) //white
+            add(Color.parseColor("#fefcec")) //cream
+            add(Color.parseColor("#e7e7e7")) //lightgray
+            add(Color.parseColor("#7a7a7a")) //darkgray
+            add(Color.parseColor("#FF000000")) //black
+
+            add(Color.parseColor("#fe820d")) //orange
+            add(Color.parseColor("#e2c79c")) //beige
+            add(Color.parseColor("#ffe600")) //yellow
+            add(Color.parseColor("#c4db88")) //lightgreen
+            add(Color.parseColor("#c5e2ff")) //skyblue
+
+            add(Color.parseColor("#ff8290")) //pink
+            add(Color.parseColor("#fee0de")) //lightpink
+            add(Color.parseColor("#00a03e")) //green
+            add(Color.parseColor("#666b16")) //kaki
+            add(Color.parseColor("#1f4ce2")) //blue
+
+            add(Color.parseColor("#ed1212")) //red
+            add(Color.parseColor("#9d2140")) //wine
+            add(Color.parseColor("#844f1e")) //brown
+            add(Color.parseColor("#7119ac")) //purple
+            add(Color.parseColor("#060350")) //navy
+
+
+
+
+
+        }
+
+
+        for (color in ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color)
+        }
+        for (color in ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color)
+        }
+
+        val dataSet: PieDataSet = PieDataSet(entries, "Expense Category")
+
+
+        dataSet.setColors(colorstwo)
+
+        val data: PieData = PieData(dataSet)
+        data.setDrawValues(true)
+        data.setValueFormatter(PercentFormatter(pieChart))
+        data.setValueTextSize(10f)
+        data.setValueTextColor(Color.BLACK)
+
+        pieChart.setData(data)
+        pieChart.invalidate()
+
+        // 애니메이션
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+
+    }
 }
