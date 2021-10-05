fba8a4faac83ab8674ba23b6b515548771887f98
HANLeeeee <hanleeeee@naver.com>
2021-10-05 19:51:42 +0900

    수인이 고장 해결

--- app/src/main/java/com/example/ehs/MainActivity.kt
+++ app/src/main/java/com/example/ehs/MainActivity.kt
@@ -7,6 +7,7 @@ import android.location.*
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowInsetsController
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -172,8 +173,6 @@ class MainActivity : AppCompatActivity() {
                     Feed_ranking()
                     loading = Loading(this)
 
-//                    feedFragment = FeedFragment.newInstance()
-//                    replaceFragment(feedFragment)
                     GlobalScope.launch(Dispatchers.Main) {
                         launch(Dispatchers.Main) {
                             loading!!.asdf()
