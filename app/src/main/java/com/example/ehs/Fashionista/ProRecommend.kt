3d1c14cce3b1e925f86e5583d5d298741d0af8f5
HANLeeeee <hanleeeee@naver.com>
2021-10-12 00:27:37 +0900

    전문가 추천에 코디등록하기 진행중 (리사이클러뷰만 만들어놈)

---
+++ app/src/main/java/com/example/ehs/Fashionista/ProRecommend.kt
@@ -0,0 +1,6 @@
+package com.example.ehs.Fashionista
+
+import android.graphics.Bitmap
+
+class ProRecommend(val procody: Bitmap){
+}
--- app/src/main/java/com/example/ehs/Fashionista/ProRecommendActivity.kt
+++ app/src/main/java/com/example/ehs/Fashionista/ProRecommendActivity.kt
@@ -1,10 +1,12 @@
 package com.example.ehs.Fashionista
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ehs.Login.AutoLogin
 import com.example.ehs.R
 import kotlinx.android.synthetic.main.activity_pro_recommend.*
@@ -13,6 +15,8 @@ class ProRecommendActivity : AppCompatActivity() {
 
     lateinit var userId : String
 
+    val proRecommendList = mutableListOf<ProRecommend>()
+    val adapter = ProRecommendyListAdapter(proRecommendList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +48,21 @@ class ProRecommendActivity : AppCompatActivity() {
         iv_proImg2.setImageBitmap(AutoLogin.StringToBitmap(proImgArr[1]))
         iv_proImg3.setImageBitmap(AutoLogin.StringToBitmap(proImgArr[2]))
 
+
+        var bitmap = BitmapFactory.decodeResource(resources, R.drawable.colortest)
+        var asdf = ProRecommend(bitmap)
+        proRecommendList.add(asdf)
+        proRecommendList.add(asdf)
+        proRecommendList.add(asdf)
+        proRecommendList.add(asdf)
+
+
+        val Linear = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rc_procody.layoutManager = Linear
+        rc_procody.setHasFixedSize(true)
+
+        rc_procody.adapter = adapter
+        adapter.notifyDataSetChanged()
     }
 
     /**
