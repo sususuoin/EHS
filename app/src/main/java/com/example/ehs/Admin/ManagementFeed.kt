31e773eded8fe0c009a2e3719906b044d4df70e4
HANLeeeee <hanleeeee@naver.com>
2021-11-23 00:01:13 +0900

    피드관리

---
+++ app/src/main/java/com/example/ehs/Admin/ManagementFeed.kt
@@ -0,0 +1,7 @@
+package com.example.ehs.Admin
+
+import android.graphics.Bitmap
+
+class ManagementFeed (val feedNo : String, val userId : String, val feedImg : Bitmap?, val date : String, val likecnt : String, val unlikecnt : String){
+
+}
